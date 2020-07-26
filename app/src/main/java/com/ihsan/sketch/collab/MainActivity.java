563b3f83bb6333c71f5c48650020b0e6e3d67456
Iyxan23 <nurihsanalghifari@gmail.com>
2020-07-26 12:25:47 +0700

    Initial Commit

---
+++ app/src/main/java/com/ihsan/sketch/collab/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ihsan.sketch.collab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
