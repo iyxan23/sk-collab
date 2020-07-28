8652acea705c54c0b48eb39a9279d49a5e87d9c8
Iyxan23 <nurihsanalghifari@gmail.com>
2020-07-28 12:22:39 +0700

    Added more activity and MenuBottomSheetDialogFragment

---
+++ app/src/main/java/com/ihsan/sketch/collab/ProfileActivity.java
@@ -0,0 +1,14 @@
+package com.ihsan.sketch.collab;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
