52566437a405524e65de615366600e17d4b6a960
Iyxan23 <nurihsanalghifari@gmail.com>
2020-07-29 16:44:51 +0700

    commit

--- app/src/main/java/com/ihsan/sketch/collab/MainFragment.java
+++ app/src/main/java/com/ihsan/sketch/collab/MainFragment.java
@@ -1,20 +1,69 @@
 package com.ihsan.sketch.collab;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class MainFragment extends Fragment {
-    private static final String TAG = "MainActivity";
+    private static final String TAG = "MainFragment";
+    private FirebaseDatabase database;
+    private FirebaseAuth auth;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        database = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser user = auth.getCurrentUser();
+        DatabaseReference userdata = database.getReference("userdata");
+
+        if (user == null) {
+            // Mod user detected
+            Intent i = new Intent(getActivity(), LoginActivity.class);
+            startActivity(i);
+            getActivity().finish();
+        }
+
+        final TextView welcome = view.findViewById(R.id.welcome_home);
+        final TextView name = view.findViewById(R.id.fullname_home);
+        TextView email = view.findViewById(R.id.email_home);
+
+        email.setText(user.getEmail());
+
+        userdata.child(user.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.child("name").exists()) {
+                    welcome.setText(getString(R.string.welcome).concat("! ").concat(snapshot.child("name").getValue(String.class)));
+                    name.setText(snapshot.child("name").getValue(String.class));
+                } else {
+                    welcome.setText(getString(R.string.welcome).concat("!"));
+                    name.setText(getString(R.string.err_set_name));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/com/ihsan/sketch/collab/RegisterActivity.java
+++ app/src/main/java/com/ihsan/sketch/collab/RegisterActivity.java
@@ -15,12 +15,17 @@ import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -52,28 +57,66 @@ public class RegisterActivity extends AppCompatActivity {
         final Button lanjut = findViewById(R.id.lanjut);
         final ProgressBar lanjut_p = findViewById(R.id.loadingLanjut);
         final Intent i = getIntent();
+        final TextInputLayout input = findViewById(R.id.name);
+        final String name = input.getEditText().getText().toString().trim();
         lanjut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (name.equals("")) {
+                    input.setError(getString(R.string.name_empty));
+                } else if (!isAsciiPrintable(name)) {
+                    input.setError(getString(R.string.name_not_ascii));
+                } else {
                 lanjut_p.setVisibility(View.VISIBLE);
                 FirebaseAuth auth = FirebaseAuth.getInstance();
                 auth.createUserWithEmailAndPassword(i.getStringExtra("email"), i.getStringExtra("password"))
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            lanjut_p.setVisibility(View.INVISIBLE);
-                            Snackbar.make(findViewById(R.id.parent_register), "GAGAL: " + e.toString(), Snackbar.LENGTH_LONG).setBackgroundTint(Color.RED).show();
-                        }
-                    }).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                        @Override
-                        public void onSuccess(AuthResult authResult) {
-                            lanjut_p.setVisibility(View.INVISIBLE);
-                            Intent i = new Intent(RegisterActivity.this, SplashActivity.class);
-                            startActivity(i);
-                            finish();
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                lanjut_p.setVisibility(View.INVISIBLE);
+                                Snackbar.make(findViewById(R.id.parent_register), "ERROR: " + e.toString(), Snackbar.LENGTH_LONG).setBackgroundTint(Color.RED).show();
+                            }
+                        }).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        lanjut_p.setVisibility(View.INVISIBLE);
+                        FirebaseDatabase database = FirebaseDatabase.getInstance();
+                        DatabaseReference userdata = database.getReference("userdata");
+                        userdata.child(authResult.getUser().getUid())
+                                .child("name")
+                                .setValue(name).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Intent i = new Intent(RegisterActivity.this, SplashActivity.class);
+                                        startActivity(i);
+                                        finish();
+                                    }
+                                }).addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(RegisterActivity.this, "WARNING: ERROR DURING SAVING YOUR NAME TO THE DATABASE, YOU MUST EDIT IT YOURSELF ON THE HOME PAGE", Toast.LENGTH_LONG)
+                                                .show();
+                                        Intent i = new Intent(RegisterActivity.this, SplashActivity.class);
+                                        startActivity(i);
+                                        finish();
+                                    }
+                                });
                         }
                     });
+                }
             }
         });
+    }
+    public static boolean isAsciiPrintable(String str) {
+        if (str == null) {
+            return false;
+        }
+        int sz = str.length();
+        for (int i = 0; i < sz; i++) {
+            if (!isAsciiPrintable(String.valueOf(str.charAt(i)))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
