f9b057a982753cbed0f08b4246c657b31fc89076
Iyxan23 <nurihsanalghifari@gmail.com>
2020-12-19 14:16:35 +0700

    Moved the android project

--- app/src/main/java/com/iyxan23/sketch/collab/LoginActivity.kt
+++ app/src/main/java/com/iyxan23/sketch/collab/LoginActivity.kt
@@ -2,6 +2,7 @@ package com.iyxan23.sketch.collab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
