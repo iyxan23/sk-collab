4c648e388a482002596b9ae2e783665582cce88f
Iyxan23 <nurihsanalghifari@gmail.com>
2020-12-20 20:22:24 +0700

    Added CheckActivity

---
+++ app/src/main/java/com/iyxan23/sketch/collab/CheckActivity.kt
@@ -0,0 +1,11 @@
+package com.iyxan23.sketch.collab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CheckActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check)
+    }
+}
