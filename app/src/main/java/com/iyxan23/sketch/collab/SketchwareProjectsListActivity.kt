1946db15a2f9bf28a63dabe4a7da7005d229c55e
Iyxan23 <nurihsanalghifari@gmail.com>
2020-12-25 09:15:17 +0700

    Implemented SketchwareProjectsListActivity, kinda

--- app/src/main/java/com/iyxan23/sketch/collab/SketchwareProjectsListActivity.kt
+++ app/src/main/java/com/iyxan23/sketch/collab/SketchwareProjectsListActivity.kt
@@ -2,10 +2,24 @@ package com.iyxan23.sketch.collab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import com.iyxan23.sketch.collab.adapters.SketchwareProjectRecyclewViewAdapter
+import com.iyxan23.sketch.collab.models.SketchwareProject
 
 class SketchwareProjectsListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sketchware_projects_list)
+
+        val sketchwareProjectsListRecyclerView: RecyclerView = findViewById(R.id.sketchware_project_list)
+
+        // Get all sketchware projects
+        val sketchwareProjects: ArrayList<SketchwareProject> = Util.getSketchwareProjects()
+
+        // Pass it onto the recyclerview
+        val adapter = SketchwareProjectRecyclewViewAdapter(sketchwareProjects, this)
+        sketchwareProjectsListRecyclerView.adapter = adapter
+
+        adapter.notifyDataSetChanged()
     }
 }
