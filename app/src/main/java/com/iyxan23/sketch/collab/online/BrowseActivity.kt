96bfb075f39474c70f599486ac15f39e4979eb02
Iyxan23 <nurihsanalghifari@gmail.com>
2021-01-02 13:43:32 +0700

    Added BrowseActivity

---
+++ app/src/main/java/com/iyxan23/sketch/collab/online/BrowseActivity.kt
@@ -0,0 +1,14 @@
+package com.iyxan23.sketch.collab.online
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iyxan23.sketch.collab.R
+
+class BrowseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_browse)
+
+        //
+    }
+}
