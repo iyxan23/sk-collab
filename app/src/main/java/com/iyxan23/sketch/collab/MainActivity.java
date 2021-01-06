757f0c37f8030a2f854b85e15c266c05218e8690
Iyxan23 <nurihsanalghifari@gmail.com>
2021-01-06 16:17:28 +0700

    Implemented MainActivity.java and added base64encode and decode in Util.java

--- app/src/main/java/com/iyxan23/sketch/collab/MainActivity.java
+++ app/src/main/java/com/iyxan23/sketch/collab/MainActivity.java
@@ -1,14 +1,156 @@
 package com.iyxan23.sketch.collab;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Pair;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.view.ViewCompat;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.*;
 
-public class MainActivity extends AppCompatActivity {
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Objects;
+
+
+class MainActivity extends AppCompatActivity {
+
+    // project keys
+    // Pair<project_key, "project" file>
+    ArrayList<Pair<String, JSONObject>> publicProjectsOwned = new ArrayList<>();
+    ArrayList<Pair<String, JSONObject>> userProjects = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Get rid of the flasing white color while doing shared element transition
+        getWindow().setEnterTransition(null);
+        getWindow().setExitTransition(null);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Check if Read and Write external storage permission is granted
+        // why scoped storage? :(
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ||
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+
+            // Tell the user first of why you need to grant the permission
+            Toast.makeText(this, "We need storage permission to access your sketchware projects. SketchCollab can misbehave if you denied the permission.", Toast.LENGTH_LONG).show();
+            // Reuest the permission(s)
+            ActivityCompat.requestPermissions(this,
+                    new String[]{
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    100);
+        }
+
+        String internalPath = getFilesDir().getAbsolutePath();
+        File lastChanges = new File(internalPath + "/last_changes");
+
+        // Get user projects, and projects that the user collaborates
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        FirebaseAuth auth  = FirebaseAuth.getInstance();
+        DatabaseReference userProjectsRef = database.getReference("/userprojects/" + auth.getUid());
+        DatabaseReference projectsRef = database.getReference("/projects/" + auth.getUid());
+
+
+        projectsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                for (DataSnapshot project : snapshot.getChildren()) {
+                    if (project.child("author").getValue(String.class).equals(auth.getUid())) {
+                        String projectBase64 = project.child("snapshot").child("project").getValue(String.class);
+                        JSONObject json = null;
+
+                        try {
+                            json = new JSONObject(Util.base64decode(projectBase64));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+
+                            // Something broke
+                            return;
+                        }
+
+                        // Because project id can vary on different devices
+                        json.optInt("sc_id");
+
+                        Pair pair = new Pair(project.getKey(), json);
+                        publicProjectsOwned.add(pair);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+                Toast.makeText(MainActivity.this,
+                            "Error while fetching data: " + error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        userProjectsRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
+                for (DataSnapshot project : snapshot.getChildren()) {
+                    String projectBase64 = project.child("snapshot").child("project").getValue(String.class);
+
+                    JSONObject json = null;
+                    try {
+                        json = new JSONObject(Util.base64decode(projectBase64));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+
+                        // Something broke
+                        return;
+                    }
+
+                    // Because project id can vary on different devices
+                    json.optInt("sc_id");
+
+                    Pair pair = new Pair(project.getKey(), json);
+                    userProjects.add(pair);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull @NotNull DatabaseError error) {
+                Toast.makeText(MainActivity.this,
+                                "Error while fetching data: " + error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // OnClicks
+        findViewById(R.id.projects_main).setOnClickListener(v -> {
+
+            ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                    MainActivity.this,
+                    findViewById(R.id.imageView8),
+                    Objects.requireNonNull( ViewCompat.getTransitionName(findViewById(R.id.imageView8)) )
+            );
+
+            Intent intent = new Intent(MainActivity.this,SketchwareProjectsListActivity.class);
+            startActivity(intent, options.toBundle());
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == PackageManager.PERMISSION_GRANTED) {
+                // TODO: Fetch sketchware projects
+            }
+        }
     }
 }
--- app/src/main/java/com/iyxan23/sketch/collab/Util.java
+++ app/src/main/java/com/iyxan23/sketch/collab/Util.java
@@ -1,4 +1,15 @@
 package com.iyxan23.sketch.collab;
 
+import android.util.Base64;
+
 public class Util {
+    public static String base64encode(String txt) {
+        byte[] data = txt.getBytes();
+        return Base64.encodeToString(data, Base64.DEFAULT);
+    }
+
+    public static String base64decode(String base64) {
+        byte[] data = Base64.decode(base64, Base64.DEFAULT);
+        return new String(data);
+    }
 }
