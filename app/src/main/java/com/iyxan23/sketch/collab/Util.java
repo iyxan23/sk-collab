3608be33acc20893f3a825cbe880359ffcdd3877
Iyxan23 <nurihsanalghifari@gmail.com>
2021-01-15 19:50:17 +0700

    Added Util.base64encode(byte[] data)

--- app/src/main/java/com/iyxan23/sketch/collab/Util.java
+++ app/src/main/java/com/iyxan23/sketch/collab/Util.java
@@ -36,6 +36,10 @@ public class Util {
         return Base64.encodeToString(data, Base64.DEFAULT);
     }
 
+    public static String base64encode(byte[] data) {
+        return Base64.encodeToString(data, Base64.DEFAULT);
+    }
+
     public static String base64decode(String base64) {
         byte[] data = Base64.decode(base64, Base64.DEFAULT);
         return new String(data);
