df3a79fd517257cdc23c48a807ee8750f2aabf24
Iyxan23 <nurihsanalghifari@gmail.com>
2021-01-15 10:08:41 +0700

    Implemented SketchwareProjectsListActivity

--- app/src/main/java/com/iyxan23/sketch/collab/SketchwareProjectsListActivity.java
+++ app/src/main/java/com/iyxan23/sketch/collab/SketchwareProjectsListActivity.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.iyxan23.sketch.collab.adapters.SketchwareProjectAdapter;
 
 public class SketchwareProjectsListActivity extends AppCompatActivity {
 
@@ -11,5 +14,14 @@ public class SketchwareProjectsListActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sketchware_projects_list);
+
+        // Get the recyclerview
+        RecyclerView projecs_list = findViewById(R.id.sketchware_project_list);
+
+        // Get the adapter
+        SketchwareProjectAdapter adapter = new SketchwareProjectAdapter(Util.fetch_sketchware_projects(), this);
+
+        // Set the adapter
+        projecs_list.setAdapter(adapter);
     }
 }
