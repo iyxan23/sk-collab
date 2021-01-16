47e3bf7b8ec014e22340e41fa4c3d52c69823937
Iyxan23 <nurihsanalghifari@gmail.com>
2021-01-16 07:41:36 +0700

    Added BrowseItemAdapter for RecyclerViews

---
+++ app/src/main/java/com/iyxan23/sketch/collab/adapters/BrowseItemAdapter.java
@@ -0,0 +1,89 @@
+package com.iyxan23.sketch.collab.adapters;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.iyxan23.sketch.collab.models.BrowseItem;
+import com.iyxan23.sketch.collab.models.SketchwareProject;
+import com.iyxan23.sketch.collab.R;
+import com.iyxan23.sketch.collab.online.UploadActivity;
+
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
+
+public class BrowseItemAdapter extends RecyclerView.Adapter<BrowseItemAdapter.ViewHolder> {
+    private static final String TAG = "BrowseItemAdapter";
+
+    private ArrayList<BrowseItem> datas = new ArrayList<>();
+    final private Activity activity;
+
+    public BrowseItemAdapter(Activity activity) {
+        this.activity = activity;
+    }
+
+    public BrowseItemAdapter(ArrayList<BrowseItem> datas, Activity activity) {
+        this.datas = datas;
+        this.activity = activity;
+    }
+
+    public void updateView(ArrayList<BrowseItem> datas) {
+        this.datas = datas;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(
+                LayoutInflater
+                        .from(parent.getContext())
+                        .inflate(R.layout.rv_item_browse, parent, false)
+        );
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
+        Log.d(TAG, "onBindViewHolder: called.");
+        BrowseItem item = datas.get(position);
+
+        holder.title.setText(item.username + "/" + item.project_name);
+
+        // https://stackoverflow.com/questions/25710457/how-to-subtract-two-calendar-object-in-android
+        // TODO: IMPLEMENT A BETTER VERSION OF THIS THING
+        Calendar now = Calendar.getInstance();
+        long difference = now.getTimeInMillis() - item.latest_commit_timestamp;
+        holder.last_updated.setText("Last Updated " + (int) (difference / (1000 * 60 * 60 * 24)) + " days ago");
+    }
+
+    @Override
+    public int getItemCount() {
+        return datas.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title;
+        TextView last_updated;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            title = itemView.findViewById(R.id.item_browse_title);
+            last_updated = itemView.findViewById(R.id.item_browse_last_updated);
+        }
+    }
+}
