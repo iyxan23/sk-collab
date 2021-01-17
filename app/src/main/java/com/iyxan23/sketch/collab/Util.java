dfdf8588ca1c5e8aa830eb19ce9ea3becc4283a8
Iyxan23 <nurihsanalghifari@gmail.com>
2021-01-17 08:23:42 +0700

    Added Util.writeFile();

--- app/src/main/java/com/iyxan23/sketch/collab/Util.java
+++ app/src/main/java/com/iyxan23/sketch/collab/Util.java
@@ -15,6 +15,7 @@ import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
@@ -240,6 +241,13 @@ public class Util {
         }
 
         return reader.array;
+    }
+
+    public static void writeFile(File file, byte[] data) throws IOException {
+        FileOutputStream outputStream = new FileOutputStream(file);
+        outputStream.write(data);
+        outputStream.flush();
+        outputStream.close();
     }
 
     public static byte[] joinByteArrays(final byte[] array1, @Nullable byte[] array2) {
