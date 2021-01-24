caff061ab70125237632da51092b36e346b49759
Iyxan23 <nurihsanalghifari@gmail.com>
2021-01-24 16:27:21 +0700

    Upload button should be disabled if it's already a sketchcollab project

--- app/src/main/java/com/iyxan23/sketch/collab/adapters/SketchwareProjectAdapter.java
+++ app/src/main/java/com/iyxan23/sketch/collab/adapters/SketchwareProjectAdapter.java
@@ -59,16 +59,27 @@ public class SketchwareProjectAdapter extends RecyclerView.Adapter<SketchwarePro
         holder.subtitle.setText(project.metadata.app_name);
         holder.details.setText(project.metadata.project_package + "(" + project.metadata.id + ")");
 
-        holder.upload_button.setOnClickListener(v -> {
-            // Go to UploadActivity
-            try {
-                Intent i = new Intent(activity.get(), UploadActivity.class);
-                i.putExtra("project_id", project.getProjectID());
-                activity.get().startActivity(i);
-            } catch (JSONException e) {
-                Toast.makeText(activity.get(), "An error occured: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        try {
+            if (project.isSketchCollabProject())
+                holder.upload_button.setEnabled(false);
+
+            else {
+                holder.upload_button.setOnClickListener(v -> {
+                    // Go to UploadActivity
+                    try {
+                        Intent i = new Intent(activity.get(), UploadActivity.class);
+                        i.putExtra("project_id", project.getProjectID());
+                        activity.get().startActivity(i);
+                    } catch (JSONException e) {
+                        Toast.makeText(activity.get(), "An error occured: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                });
             }
-        });
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(activity.get(), "An error occured: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
