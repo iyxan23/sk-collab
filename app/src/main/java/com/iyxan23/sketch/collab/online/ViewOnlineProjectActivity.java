b8603db22cc5381afc695929472b97ed76d018f7
Iyxan23 <nurihsanalghifari@gmail.com>
2021-01-25 16:20:06 +0700

    Added ViewOnlineProjectActivity

---
+++ app/src/main/java/com/iyxan23/sketch/collab/online/ViewOnlineProjectActivity.java
@@ -0,0 +1,42 @@
+package com.iyxan23.sketch.collab.online;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.iyxan23.sketch.collab.R;
+import com.iyxan23.sketch.collab.online.databinding.ActivityViewOnlineProjectBinding;
+
+public class ViewOnlineProjectActivity extends AppCompatActivity {
+
+    private ActivityViewOnlineProjectBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityViewOnlineProjectBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+        toolBarLayout.setTitle(getTitle());
+
+        FloatingActionButton fab = binding.fab;
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
