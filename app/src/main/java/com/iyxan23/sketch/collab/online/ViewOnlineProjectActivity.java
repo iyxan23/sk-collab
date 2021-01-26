1acf7ce34c2d20dd961b5933652d6e028deb95ec
Iyxan23 <nurihsanalghifari@gmail.com>
2021-01-26 16:14:46 +0700

    Unfinished changes

--- app/src/main/java/com/iyxan23/sketch/collab/online/ViewOnlineProjectActivity.java
+++ app/src/main/java/com/iyxan23/sketch/collab/online/ViewOnlineProjectActivity.java
@@ -2,17 +2,25 @@ package com.iyxan23.sketch.collab.online;
 
 import android.os.Bundle;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.iyxan23.sketch.collab.R;
 import com.iyxan23.sketch.collab.databinding.ActivityViewOnlineProjectBinding;
+
+import org.jetbrains.annotations.NotNull;
 
 public class ViewOnlineProjectActivity extends AppCompatActivity {
 
@@ -30,12 +38,21 @@ public class ViewOnlineProjectActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
-        toolBarLayout.setTitle(getTitle());
-
         FloatingActionButton fab = binding.fab;
         fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
+
+        String project_key = getIntent().getStringExtra("project_key");
+
+        // Fetch the project from the database
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        DocumentReference project = firestore.collection("projects").document(project_key);
+
+        project
+                .get()
+                .addOnCompleteListener(task -> {
+                    
+                });
     }
 
     @Override
