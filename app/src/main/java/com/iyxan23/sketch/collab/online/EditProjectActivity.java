d7e5547180e7112009921f705fde6916ade757c8
Iyxan23 <nurihsanalghifari@gmail.com>
2021-01-27 17:16:13 +0700

    Add a dark overlay so we can read the white texts

---
+++ app/src/main/java/com/iyxan23/sketch/collab/online/EditProjectActivity.java
@@ -0,0 +1,14 @@
+package com.iyxan23.sketch.collab.online;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditProjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_project);
+    }
+}
