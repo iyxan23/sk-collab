522165b9d47e4b334709b175f79c4f3ae67de917
Iyxan23 <nurihsanalghifari@gmail.com>
2021-01-31 15:40:29 +0700

    fix: Move the clone into onStartCommand on a different thread

--- app/src/main/java/com/iyxan23/sketch/collab/services/CloneService.java
+++ app/src/main/java/com/iyxan23/sketch/collab/services/CloneService.java
@@ -6,13 +6,9 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
-import android.os.Process;
 import android.os.Build;
-import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.IBinder;
-import android.os.Looper;
-import android.os.Message;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -23,7 +19,7 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.iyxan23.sketch.collab.R;
 import com.iyxan23.sketch.collab.Util;
@@ -43,11 +39,10 @@ import name.fraser.neil.plaintext.diff_match_patch;
 
 public class CloneService extends Service {
 
+    private static final String TAG = "CloneService";
+
     private final String CLONE_CHANNEL_ID = "CLONE_CHANNEL";
     private final int NOTIFICATION_ID = 1;
-
-    private Looper serviceLooper;
-    private ServiceHandler serviceHandler;
 
     String project_key;
     String project_name;
@@ -58,16 +53,46 @@ public class CloneService extends Service {
         return null;
     }
 
-    // Handler that receives messages from the thread
-    private final class ServiceHandler extends Handler {
-        public ServiceHandler(Looper looper) {
-            super(looper);
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        project_key = intent.getStringExtra("project_key");
+        project_name = intent.getStringExtra("project_name");
+
+        Intent notificationIntent = new Intent(this, ViewOnlineProjectActivity.class);
+        notificationIntent.putExtra("project_key", project_key);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        final Notification.Builder notification_builder;
+
+        // Channel ID is for 26+ / Android 8+ / Android Oreo+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createCloneNotificationChannel();
+
+            notification_builder = new Notification.Builder(this, CLONE_CHANNEL_ID);
+        } else {
+            notification_builder = new Notification.Builder(this);
         }
 
-        @Override
-        public void handleMessage(Message msg) {
-            Toast.makeText(CloneService.this, "Cloning started.", Toast.LENGTH_SHORT).show();
-            
+        Notification notification = notification_builder
+                                        .setContentTitle("Cloning Project")
+                                        .setContentText("Cloning " + project_name + " (" + project_key + ")")
+                                        .setProgress(100, 1, true)
+                                        .setSmallIcon(R.drawable.ic_file_download)
+                                        .setContentIntent(pendingIntent)
+                                        .build();
+
+        startForeground(NOTIFICATION_ID, notification);
+
+        // Start the cloning on a different thread
+        Toast.makeText(CloneService.this, "Cloning started.", Toast.LENGTH_SHORT).show();
+        new Thread(() -> {
             try {
                 FirebaseFirestore firestore = FirebaseFirestore.getInstance();
                 DocumentReference   project = firestore.collection("projects").document(project_key);
@@ -156,59 +181,10 @@ public class CloneService extends Service {
                 Thread.currentThread().interrupt();
             }
 
-            Toast.makeText(CloneService.this, "Clone " + project_name + " finished", Toast.LENGTH_SHORT).show();
-
             // Stop the service using the startId, so that we don't stop
             // the service in the middle of handling another job
-            stopSelf(msg.arg1);
-        }
-    }
-
-    @Override
-    public void onCreate() {
-        // Start up the thread running the service. Note that we create a
-        // separate thread because the service normally runs in the process's
-        // main thread, which we don't want to block. We also make it
-        // background priority so CPU-intensive work doesn't disrupt our UI.
-        HandlerThread thread = new HandlerThread("Clone Thread",
-                Process.THREAD_PRIORITY_BACKGROUND);
-        thread.start();
-
-        // Get the HandlerThread's Looper and use it for our Handler
-        serviceLooper = thread.getLooper();
-        serviceHandler = new ServiceHandler(serviceLooper);
-    }
-
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        project_key = intent.getStringExtra("project_key");
-        project_name = intent.getStringExtra("project_name");
-
-        Intent notificationIntent = new Intent(this, this.getClass());
-        PendingIntent pendingIntent =
-                PendingIntent.getActivity(this, 0, notificationIntent, 0);
-
-        final Notification.Builder notification_builder;
-
-        // Channel ID is for 26+ / Android 8+ / Android Oreo+
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            createCloneNotificationChannel();
-
-            notification_builder = new Notification.Builder(this, CLONE_CHANNEL_ID);
-        } else {
-            notification_builder = new Notification.Builder(this);
-        }
-
-        Notification notification = notification_builder
-                                        .setContentTitle("Cloning Project")
-                                        .setContentText("Cloning " + project_name + " (" + project_key + ")")
-                                        .setProgress(100, 1, true)
-                                        .setSmallIcon(R.drawable.ic_file_download)
-                                        .setContentIntent(pendingIntent)
-                                        .build();
-
-        startForeground(NOTIFICATION_ID, notification);
+            stopSelf();
+        }).start();
 
         return START_NOT_STICKY;
     }
