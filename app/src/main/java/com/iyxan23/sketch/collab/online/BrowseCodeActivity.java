60dbf21a4b68fd439ee40bf4f56fdcb70ee0459e
Iyxan23 <nurihsanalghifari@gmail.com>
2021-02-02 13:35:52 +0700

    add: Add BrowseCodeActivity

---
+++ app/src/main/java/com/iyxan23/sketch/collab/online/BrowseCodeActivity.java
@@ -0,0 +1,16 @@
+package com.iyxan23.sketch.collab.online;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.iyxan23.sketch.collab.R;
+
+public class BrowseCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browse_code);
+    }
+}
