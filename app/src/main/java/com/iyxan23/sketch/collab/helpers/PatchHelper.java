f32ea97b7d8a5dafd2099f4e6483b49f3684f58f
Iyxan23 <nurihsanalghifari@gmail.com>
2021-02-06 15:00:12 +0700

    add: Add reverse_patch()

--- app/src/main/java/com/iyxan23/sketch/collab/helpers/PatchHelper.java
+++ app/src/main/java/com/iyxan23/sketch/collab/helpers/PatchHelper.java
@@ -1,4 +1,28 @@
 package com.iyxan23.sketch.collab.helpers;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PatchHelper {
+
+    public static String reverse_patch(String patch) {
+        Pattern plus_pattern = Pattern.compile("^\\+", Pattern.MULTILINE);
+        Pattern minus_pattern = Pattern.compile("^-", Pattern.MULTILINE);
+
+        StringBuilder patch_sb = new StringBuilder(patch);
+
+        // Flip + to -
+        Matcher matcher = plus_pattern.matcher(patch);
+        while (matcher.find()) {
+            patch_sb.setCharAt(matcher.start(), '-');
+        }
+
+        // Flip - to +
+        matcher = minus_pattern.matcher(patch);
+        while (matcher.find()) {
+            patch_sb.setCharAt(matcher.start(), '+');
+        }
+
+        return patch_sb.toString();
+    }
 }
