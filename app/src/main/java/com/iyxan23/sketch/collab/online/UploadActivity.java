dff0813152b0a4cbdd158670e993cdf5fb1e6eb1
Iyxan23 <nurihsanalghifari@gmail.com>
2021-02-11 14:00:38 +0700

    fix: Import android.util.Log

--- app/src/main/java/com/iyxan23/sketch/collab/online/UploadActivity.java
+++ app/src/main/java/com/iyxan23/sketch/collab/online/UploadActivity.java
@@ -6,6 +6,7 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
